a27322fe888e828416467b0889ac1cdc539776b5
jobrie12 <jamesbryantobrien@gmail.com>
2017-07-14 19:01:25 -0400

    More Updates

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by james on 6/24/2017.
  */
-var app = angular.module('oneiros', ['ui.router']);
+var app = angular.module('oneiros', ['ui.router', 'vcRecaptcha']);
 
 app.config(['$locationProvider', function($locationProvider) {
     $locationProvider.hashPrefix('');
@@ -138,7 +138,7 @@ app.controller('AboutCtrl', [
                 strokeWidth: 2,
                 trailWidth: 1,
                 easing: 'easeInOut',
-                duration: 3400,
+                duration: 2000,
                 svgStyle: {width: '100%', height: '100%'},
                 text: {
                     style: {
@@ -183,12 +183,15 @@ app.controller('AboutCtrl', [
             var bar2 = new ProgressBar.Line('#container2', setup2);
             var bar3 = new ProgressBar.Line('#container3', setup3);
             var bar4 = new ProgressBar.Line('#container4', setup4);
+            var bar5 = new ProgressBar.Line('#container5', setup4);
             bar.animate(.75);  // Number from 0.0 to 1.0
             bar2.animate(105/300.0);  // Number from 0.0 to 1.0
             bar3.animate(13/15.0);  // Number from 0.0 to 1.0
 
             var dayAgo = Date.create('October 6, 2016').daysAgo();
             bar4.animate(dayAgo/365.0);  // Number from 0.0 to 1.0
+            var dayAgo = Date.create('December 9, 2016').daysAgo();
+            bar5.animate(dayAgo/365.0);  // Number from 0.0 to 1.0
         }
 
         init();
@@ -199,7 +202,8 @@ app.controller('ContactCtrl', [
     '$state',
     '$window',
     '$http',
-    function($scope, $state, $window, $http){
+    'vcRecaptchaService',
+    function($scope, $state, $window, $http, vcRecaptchaService){
         $scope.followUpOptions = ['E-mail', 'Call', 'Text'];
         $scope.serviceOptions = [
             {name:'New Website', value:false},
@@ -219,6 +223,8 @@ app.controller('ContactCtrl', [
         $scope.followUp = "";
         $scope.other = "";
         $scope.services = "";
+        $scope.captchaValue = null;
+        $scope.publicKey = "6LdqFScUAAAAAOcHxta8OI1DCCbF5IJ_iRT8ApQD";
 
         function init(){
             $(document).ready(function() {
@@ -247,9 +253,12 @@ app.controller('ContactCtrl', [
             $scope.followUp = "";
             $scope.other = "";
             $scope.services = "";
+            $scope.captchaValue = null;
         }
 
         $scope.generateEmail = function(){
+
+            console.log($scope.captchaValue);
 
             $scope.serviceOptions.forEach(function(n){
                if(n.value){
@@ -261,9 +270,6 @@ app.controller('ContactCtrl', [
 
             if ($scope.name.length < 1){
                 toast += " name,"
-            }
-            if ($scope.business.length < 1) {
-                toast += " business,"
             }
             if ($scope.email.length < 1) {
                 toast += " email,"
@@ -280,7 +286,9 @@ app.controller('ContactCtrl', [
                 Materialize.toast("Please fill out the " + toast + " fields before submitting.", 5000, 'rounded red')
             }else if ($scope.followUp != "E-Mail" && $scope.phone.length < 1){
                 Materialize.toast("If you want us to follow up by phone, we will need a phone #", 5000, 'rounded red')
-            }
+            } /*else if(vcRecaptchaService.getResponse() === ""){
+                Materialize.toast("Please resolve the captcha before submitting", 5000, 'rounded red')
+            }*/
             else {
                 $scope.services = $scope.services.slice(0, -2);
                 $http({
@@ -295,6 +303,7 @@ app.controller('ContactCtrl', [
                         followUp: $scope.followUp,
                         services: $scope.services,
                         other: $scope.other
+                        //gcaptcharesponse: vcRecaptchaService.getResponse()
                     }
                 }).then(function successCallback(response) {
                         if (response.data.indexOf('Failed') != -1){
@@ -305,6 +314,7 @@ app.controller('ContactCtrl', [
                             clearForm();
                         }
                     }, function errorCallback(response) {
+                        console.log(response.message);
                         // called asynchronously if an error occurs
                         // or server returns response with an error status.
                         Materialize.toast("ERROR: " + response.data, 5000, 'rounded red')
@@ -398,6 +408,139 @@ app.controller('chessiesCtrl', [
         function init(){
 
         }
+
+        init();
+    }]
+);
+
+app.controller('demo-firmCtrl', [
+    '$scope',
+    '$state',
+    '$location',
+    function($scope, $state, $location){
+
+        $scope.projects = [
+            {
+                "name":"abbot",
+                "title": "Abbot Residence",
+                "image": "images/abbot.jpg"
+            },
+            {
+                "name":"coffin",
+                "title": "Coffin Residence",
+                "image": "images/coffin.jpg"
+            },
+            {
+                "name":"ellison",
+                "title": "Ellison Residence",
+                "image": "images/ellison.jpg"
+            }
+        ];
+
+        $scope.isActive = function (name){
+           if( $location.url().indexOf(name) != -1){
+                return 'active';
+           } else if (name === 'home' && $location.url().split("/").length - 1 == 2){
+               return 'active';
+           }
+        };
+
+        function init(){
+
+        }
+
+        init();
+    }]
+);
+
+app.controller('ubblsaCtrl', [
+    '$scope',
+    '$state',
+    '$location',
+    function($scope, $state, $location){
+
+        $scope.followUpOptions = ['E-mail', 'Call', 'Text'];
+        $scope.serviceOptions = [
+            {name:'New Website', value:false},
+            {name:'Website Redesign', value:false},
+            {name:'Logo/Graphic Design', value:false},
+            {name:'Eat24 Online Order Integration', value:false},
+            {name:'E-Commerce', value:false},
+            {name:'E-Mail Outreach', value:false},
+            {name:'Web Training', value:false},
+            {name:'Other', value:false}
+        ];
+        $scope.name = "";
+        $scope.phone = "";
+        $scope.email = "";
+        $scope.website = "";
+        $scope.business = "";
+        $scope.followUp = "";
+        $scope.other = "";
+        $scope.services = "";
+        $scope.captchaValue = null;
+        $scope.publicKey = "6LdqFScUAAAAAOcHxta8OI1DCCbF5IJ_iRT8ApQD";
+
+        function init(){
+            $(document).ready(function() {
+                $('select').material_select();
+            });
+        }
+
+        function clearForm() {
+            $scope.serviceOptions = [
+                {name:'New Website', value:false},
+                {name:'Website Redesign', value:false},
+                {name:'Logo/Graphic Design', value:false},
+                {name:'Eat24 Online Order Integration', value:false},
+                {name:'E-Commerce', value:false},
+                {name:'E-Mail Outreach', value:false},
+                {name:'Web Training', value:false},
+                {name:'Other', value:false}
+            ];
+            $scope.name = "";
+            $scope.phone = "";
+            $scope.email = "";
+            $scope.website = "";
+            $scope.business = "";
+            $scope.followUp = "";
+            $scope.other = "";
+            $scope.services = "";
+        }
+
+        $scope.generateEmail = function(){
+
+            $scope.serviceOptions.forEach(function(n){
+                if(n.value){
+                    $scope.services += n.name + " | "
+                }
+            });
+
+            var toast = "";
+
+            if ($scope.name.length < 1){
+                toast += " name,"
+            }
+            if ($scope.email.length < 1) {
+                toast += " email,"
+            }
+
+            if (toast.length > 0){
+                toast = toast.slice(0, -1);
+                Materialize.toast("Please fill out the " + toast + " fields before submitting.", 5000, 'rounded red')
+            } else {
+                Materialize.toast("Thanks for the email, we'll get back to you shortly!", 5000, 'rounded green');
+                clearForm();
+            }
+        };
+
+        $scope.isActive = function (name){
+            if( $location.url().indexOf(name) != -1){
+                return 'active';
+            } else if (name === 'home' && $location.url().split("/").length - 1 == 2){
+                return 'active';
+            }
+        };
 
         init();
     }]
